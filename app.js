d6d99475bd47bca4adb780fa70d7afd0ecb1b23b
Gwyn Jones <m@gwnjns.com>
2019-04-05 15:12:08 +0100

    Introduce basic HTTP service

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const data = require('./refs_in_guides_backlinked');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res, next) => {
+
+    const body = [];
+
+    Object.keys(data).forEach((key, index) => {
+        body.push(`<a href="/${key}">${key}</a>&nbsp;`);
+    });
+
+    res.send(body.join(' '));
+
+});
+
+app.get('/:iaid', (req, res, next) => {
+    res.send(data[req.params.iaid]);
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('CORS-enabled web server listening on port 80')
+})
