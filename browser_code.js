6a138e7f1d035f8a14a2089fe3d5e8b6db7dce18
Gwyn Jones <m@gwnjns.com>
2019-04-10 12:53:29 +0100

    Update browser code to explain when no guides are found

--- browser_code.js
+++ browser_code.js
@@ -1,6 +1,14 @@
 fetch(`https://mighty-island-64939.herokuapp.com/${window.location.href.match(/([^/])*$/)[0]}`)
-    .then(response => response.json())
+    .then(response => {
+        return response.json();
+    })
     .then(data => {
+
+        if (data.noting_found) {
+            $('main').append(`<div class="details-row" style="padding: 3em; background: #DDD; border: 5px solid #333; margin-bottom: 1em;">${data.response}</div>`);
+            throw data.response;
+        }
+
 
         fetch('https://vast-lowlands-86122.herokuapp.com/', {
             method: 'POST',
@@ -12,7 +20,6 @@ fetch(`https://mighty-island-64939.herokuapp.com/${window.location.href.match(/(
         })
             .then(response => response.text())
             .then(text => {
-                console.log(text);
 
                 // Not great practice, but we know jQuery is available on Discovery details pages
                 $('main').append(`<div style="padding: 3em; background: #DDD; border: 5px solid #333">${text}</div>`);
